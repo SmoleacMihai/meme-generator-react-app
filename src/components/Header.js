f805cf9c943b505d53b7910fb3cf9f208bce74c9
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-09-18 17:45:01 +0300

    Started project, made header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header--logo">
+        <img src="./images/Troll-Face.png" alt="trololo" className="logo--troll-face"/>
+        <span className="logo--title">Meme Generator</span>
+      </div>
+      <h3 className="header--title">React Course - Project 3</h3>
+    </div>
+  )
+}
+
+export default Header;
